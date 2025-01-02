e311732cc0ac9b54cbb392b29ef35105d4f8a53c
Tejaswini <129767798+JTejaswini89@users.noreply.github.com>
2025-01-02 20:19:28 +0530

    Type Casting

---
+++ TypeCasting.java
@@ -0,0 +1,8 @@
+public class TypeCasting{
+public static void main(String args[]){
+int intType=20;
+short shortType=(short)intType;
+System.out.println("Int type:" +intType);
+System.out.println("Short type:" +shortType);
+}
+}
