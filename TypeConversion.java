82cf4586cb777242bc33a978c0277ec8f9aaf0a2
Tejaswini <129767798+JTejaswini89@users.noreply.github.com>
2025-01-02 20:13:59 +0530

    Type Conversions

---
+++ TypeConversion.java
@@ -0,0 +1,8 @@
+public class TypeConversion{
+public static void main(String args[]){
+int intType=20;
+float floatType=intType;
+System.out.println("Int type:"+intType);
+System.out.println("FloatType:"+floatType);
+}
+}
