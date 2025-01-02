6d274f8fb7372c52b799c4bb8313739f41138a12
Tejaswini <129767798+JTejaswini89@users.noreply.github.com>
2025-01-02 20:08:41 +0530

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -15,8 +15,26 @@ System.out.println("Division:" +(num1/num2));
 System.out.println("Modulus:" +(num1%num2));
 System.out.println("Increment:" +(num1++));
 System.out.println("Decrement:" +(num1--));
+System.out.println("Increment:" +(num2++));
+System.out.println("Decrement:" +(num2--));
+
+//Assignment Operators
+System.out.println("Assign:" +(num1=num2));
+System.out.println("Add and Assign:" +(num1+=num2)); 
+System.out.println("Subtract and Assign:" +(num1-=num2)); 
+System.out.println("Multiply and Assign:" +(num1*=num2)); 
+System.out.println("Divide and Assign:" +(num1/=num2)); 
+System.out.println("Modulus and Assign:" +(num1%=num2)); 
+
+//Comparison Operator
+System.out.println("Is Equal To:" +(num1==num2));
+System.out.println("Is Not Equal To:" +(num1!=num2)); 
+System.out.println("Is Greaterthan To:" +(num1>num2)); 
+System.out.println("Is Lessthan To:" +(num1<num2)); 
+System.out.println("Is Greaterthan or Lessthan To:" +(num1>=num2)); 
+System.out.println("Is Lessthan or equal To:" +(num1<=num2)); 
+ 
+sc.close();
 }
 }
-
-
 
